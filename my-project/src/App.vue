4f9a3fc4e31af02e5d0cf7331230389c57002a8f
shironekoss <felisgosal1996@gmail.com>
2023-05-14 16:51:52 +0700

    Scrollbar CSS

---
+++ my-project/src/App.vue
@@ -0,0 +1,43 @@
+<template>
+<div :class="{ 'dark-mode': isDarkMode }">
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+ <Nestedoverflow/>
+</div>
+
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import globalcss from './mycss.css'
+import Nestedoverflow from './components/Nestedoverflow'
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    Nestedoverflow
+  },
+  computed: {
+    isDarkMode() {
+      return true
+    },
+    styles() {
+      return this.isDarkMode ? globalcss : 'lightStyles';
+    }
+  },
+   created() {
+    this.$options.style = this.styles;
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ my-project/src/components/Nestedoverflow.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="parent" :style="{ height: parentHeight + 'px' }">
+    <h1>aaaaaa</h1>
+    <h1>bbbbbbb</h1>
+    <h1>aaaaaa</h1>
+    <h1>bbbbbbb</h1>
+    <h1>aaaaaa</h1>
+    <h1>bbbbbbb</h1>
+    <div class="child" :style="{ height: childHeight + 'px' }">
+      <h1>aaaaaa</h1>
+      <h1>bbbbbbb</h1>
+      <h1>aaaaaa</h1>
+      <h1>bbbbbbb</h1>
+      <h1>aaaaaa</h1>
+      <h1>bbbbbbb</h1>
+      <div class="nested" :style="{ height: nestedHeight + 'px' }">
+        <h1>aaaaaa</h1>
+        <h1>bbbbbbb</h1>
+        <h1>aaaaaa</h1>
+        <h1>bbbbbbb</h1>
+        <h1>aaaaaa</h1>
+        <h1>bbbbbbb</h1>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NestedOverflowContainer",
+  data() {
+    return {
+      parentHeight: 250,
+      childHeight: 200,
+      nestedHeight: 150,
+    };
+  },
+};
+</script>
+
+<style>
+.parent {
+  border: 1px solid #ccc;
+  overflow: auto;
+}
+
+.parent::-webkit-scrollbar {
+  width: 16px;
+}
+
+.parent::-webkit-scrollbar-track {
+  border-radius: 8px;
+  background-color: #e7e7e7;
+  border: 1px solid #cacaca;
+}
+
+.parent::-webkit-scrollbar-thumb {
+  border-radius: 8px;
+  border: 3px solid transparent;
+  background-clip: content-box;
+  background-color: #d55959;
+}
+
+.child {
+  border: 1px solid blue;
+  overflow: auto;
+}
+
+.nested {
+  border: 1px solid green;
+  overflow: auto;
+}
+</style>
